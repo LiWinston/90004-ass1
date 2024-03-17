671fe975598f42a86941bc0e5ee35866b6093092
ycl_MBP2022 <leeyc.official@gmail.com>
2024-03-17 18:01:10 +1100

    格式化

--- src/Params.java
+++ src/Params.java
@@ -12,13 +12,13 @@ public class Params {
   public final static String LOG_FILE = "log.txt";
 
   //the number of nurses in the emergency department
-  public final static int NURSES = 3;
+  public final static int NURSES = 5;
   
   //the number of orderlies in the emergency department
-  public final static int ORDERLIES = 8;
+  public final static int ORDERLIES = 4;
   
   //the number of orderlies required to transfer a patient
-  public final static int TRANSFER_ORDERLIES = 3;
+  public final static int TRANSFER_ORDERLIES = 2;
   
   //the proportion of patients whose condition is severe
   public final static double SEVERE_PROPORTION = 0.65;
@@ -30,13 +30,13 @@ public class Params {
   public final static int TREATMENT_TIME = 800;
   
   //the time it takes for a nurse (+ orderlies) to transfer a patient between locations
-  public final static int TRANSFER_TIME = 200;
+  public final static int TRANSFER_TIME = 2000;
   
   //the time a specialist spends elsewhere between treating patients
   public final static int SPECIALIST_AWAY_TIME = 500;
   
   //the maximum amount of time between patient arrivals at ED (before admission)
-  public final static int MAX_ARRIVE_INTERVAL = 2000;
+  public final static int MAX_ARRIVE_INTERVAL = 500;
 
   //the maximum amount of time between departures from ED (after discharge)
   public final static int MAX_DEPART_INTERVAL = 500;
--- src/Producer.java
+++ src/Producer.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 /**
  * Produces new patients to present to the emergency department.
  *
@@ -9,25 +11,39 @@
 
 public class Producer extends Thread {
 
-	private Foyer foyer;
-	
+    private Foyer foyer;
+    private LinkedList<Patient> patients = new LinkedList<Patient>();
+
     // create a new producer
     Producer(Foyer newFoyer) {
         this.foyer = newFoyer;
     }
 
-    // quests 
+    // quests
     public void run() {
-        while(!isInterrupted()) {
+        while (!isInterrupted()) {
             try {
-                // create a new patient and admit it to the ED.
+                // create a new patient and add it to the queue
                 Patient patient = Patient.getNewPatient();
-                this.foyer.admitToEd(patient);
+                patients.addLast(patient);
+
+                // try admitting patients from the queue
+                tryAdmitPatients();
 
                 // let some time pass before the next patient is admitted
                 sleep(Params.arrivalPause());
             } catch (InterruptedException e) {
                 this.interrupt();
+            }
+        }
+    }
+
+    // try admitting patients from the queue
+    private void tryAdmitPatients() throws InterruptedException {
+        synchronized (patients) {
+            if (!patients.isEmpty()) {
+                Patient nextPatient = patients.removeFirst();
+                this.foyer.admitToEd(nextPatient);
             }
         }
     }
